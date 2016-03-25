761dc913043990f1d210e18c3f2eff11e7d75993
Xiaoxi Zheng <shoush0713@gmail.com>
2016-03-25 16:05:24 -0400

    begin restructuring and triggered music on home

---
+++ Assets/scripts/GUI/AudioScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioScript : MonoBehaviour {
+	//public AudioClip openTheme;
+	public AudioSource source;
+	// Use this for initialization
+	void Awake () {
+		source = GetComponent<AudioSource>();
+	}
+	void Start(){
+		source.Play();
+		//source.Play(44100);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
