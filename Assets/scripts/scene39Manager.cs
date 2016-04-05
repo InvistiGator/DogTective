a6d0f23fc056c3521feb4c334eb2d017045ead2d
Sarah Klein <Ssk13@ufl.du>
2016-04-05 00:34:27 -0400

    scene39 sound fx

--- Assets/scripts/scene39Manager.cs
+++ Assets/scripts/scene39Manager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class scene39Manager : MonoBehaviour {
 	public GameObject SceneHandlerObj;
 	public SceneHandler sceneManagerScript;
+	public audio39Script audioManager;
 	public Text displayedDialogue =  null;
 	
 	private string [] dialogue_1;
@@ -27,6 +28,7 @@ public class scene39Manager : MonoBehaviour {
 		SceneHandlerObj = GameObject.FindGameObjectWithTag("SceneHandlerM") as GameObject;
 		//finds the script that is attached to the above gameobject
 		sceneManagerScript = SceneHandlerObj.GetComponent<SceneHandler>();
+		audioManager = audioManager.GetComponent<audio39Script>();
 	}
 	void Start () {
 		sceneManagerScript.setUserVisited(39);
@@ -70,6 +72,13 @@ public class scene39Manager : MonoBehaviour {
 	
 	public void displayDialogue(){
 		if(section == 1 && i < dialogue_1Length){
+			if (i==9) {
+				audioManager.playKnocking();
+			} else if (i==13) {
+				audioManager.playDoorOpen();
+			} else if (i==32) {
+				audioManager.playDoorClose();
+			}
 			displayedDialogue.text = dialogue_1[i];
 			i++;
 		}
