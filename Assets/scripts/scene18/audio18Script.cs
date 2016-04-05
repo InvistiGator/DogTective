715595565e9635bd6a3db396e668743adbdd3784
Sarah Klein <Ssk13@ufl.du>
2016-04-04 22:51:19 -0400

    scene18 sound fx

---
+++ Assets/scripts/scene18/audio18Script.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class audio18Script : MonoBehaviour {
+	//
+	public AudioSource audio;
+
+	public AudioClip decisionResolution;
+	public AudioClip decisionPrompt;
+
+	// Use this for initialization
+	void Awake () {
+		audio = GetComponent<AudioSource>();
+	}
+	void Start(){
+		
+	}
+
+	public void playDecisionRes() {
+		audio.clip=decisionResolution;
+		audio.Play();
+	}
+	public void playDecisionPrompt() {
+		audio.clip=decisionPrompt;
+		audio.Play();
+	}
+
+}
