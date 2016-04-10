1df18c30b35a6a3086c10c4204b306741ec92972
Xiaoxi Zheng <shoush0713@gmail.com>
2016-04-10 01:11:45 -0400

    put damina in scene1 1 with animation trigger

---
+++ Assets/Damina/DaminaEmotionController.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class DaminaEmotionController : MonoBehaviour {
+
+	static Animator anim;
+	public GameObject Damina;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+
+		Damina.SetActive(false);
+
+	}
+	void Update(){
+
+	}
+	public void isIdle(){
+		anim.SetTrigger("isIdle"); 
+	}
+	public void isSuprised(){
+		anim.SetTrigger("isSuprised");
+	}
+	public void isSad(){
+		anim.SetTrigger("isSad");
+	}
+	public void isHappy(){
+		anim.SetTrigger("isHappy");
+	}
+	public void isAwk(){
+		anim.SetTrigger("isAwk");
+	}
+	public void isAnnoyed(){
+		anim.SetTrigger("isAnnoyed");
+	}
+	public void isAngry(){
+		anim.SetTrigger("isAngry");
+	}
+	public void enter(){
+		Damina.SetActive(true);
+	}
+	public void exit(){
+		Damina.SetActive(false);
+	}
+}
