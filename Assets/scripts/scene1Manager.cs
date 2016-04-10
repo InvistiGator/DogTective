8b4cb2362aa8e8df7c0905822cfba86488c25d73
rumakn <rumakn1414@gmail.com>
2016-04-10 17:39:28 -0400

    Doug in scene 7 + collide

--- Assets/scripts/scene1Manager.cs
+++ Assets/scripts/scene1Manager.cs
@@ -61,7 +61,7 @@ public class scene1Manager : MonoBehaviour {
 			//emoCtr1.triggerIdle();
 		} //else if(i==maxDialogueLength+1){
 		else if(i==2){
-			StartCoroutine(sceneManagerScript.FadeStuff(16));
+			StartCoroutine(sceneManagerScript.FadeStuff(7));
 		}
 	}
 
