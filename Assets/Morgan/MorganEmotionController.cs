d2bc6c9f48dea5c274ee24508546eeff23f7a385
Xiaoxi Zheng <shoush0713@gmail.com>
2016-04-10 03:26:22 -0400

    pushing for updates

---
+++ Assets/Morgan/MorganEmotionController.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MorganEmotionController : MonoBehaviour {
+
+	static Animator anim;
+	public GameObject Morgan;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+
+		Morgan.SetActive(true);
+
+	}
+	void Update(){
+
+	}
+	public void isIdle(){
+		anim.SetTrigger("isIdle"); 
+	}
+	public void isSuprised(){
+		anim.SetTrigger("isSuprised");
+	}
+	public void isSad(){
+		anim.SetTrigger("isSad");
+	}
+	public void isHappy(){
+		anim.SetTrigger("isHappy");
+	}
+	public void isAwk(){
+		anim.SetTrigger("isAwk");
+	}
+	public void isAnnoyed(){
+		anim.SetTrigger("isAnnoyed");
+	}
+	public void isAngry(){
+		anim.SetTrigger("isAngry");
+	}
+	public void enter(){
+		Morgan.SetActive(true);
+	}
+	public void exit(){
+		Morgan.SetActive(false);
+	}
+}
